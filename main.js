bcdd957f53a6e78cb0ef362ba04a5a0297f40a5c
Bhavishyaa Vignesh <67924047+bhavicodey@users.noreply.github.com>
2022-02-20 14:33:53 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+video = "";
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380)
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Object has dectected!!!!!!";
+}
+
+function modelLoaded() {
+    console.log("Model has loaded!!")
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
